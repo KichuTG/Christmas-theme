83f03650c9609ccab1a1f78b9a9c2cba29b12d4a
Kichu <kichumon333@outlook.com>
2024-12-22 02:45:17 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -97,7 +97,7 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                   text=f"⌾{get_size(file.file_size)}⎙{file.file_name}", callback_data=f'{pre}#{file.file_id}'
+                   text=f"🎄{get_size(file.file_size)}⛄️{file.file_name}", callback_data=f'{pre}#{file.file_id}'
                 ),
             ]
             for file in files
@@ -119,16 +119,16 @@ async def next_page(bot, query):
         if settings['auto_delete']:
             btn.insert(0, 
             [
-                InlineKeyboardButton(f'✢ ɪɴꜰᴏ ✢', 'tips'),
-                InlineKeyboardButton(f'✢ ᴛɪᴘꜱ ✢', 'info')
+                InlineKeyboardButton(f'🧣 ɪɴꜰᴏ 🧣', 'tips'),
+                InlineKeyboardButton(f'🧸 ᴛɪᴘꜱ 🧸', 'info')
             ]
             )
 
         else:
             btn.insert(0, 
             [
-                InlineKeyboardButton(f'✢ ɪɴꜰᴏ ✢', 'tips'),
-                InlineKeyboardButton(f'✢ ᴛɪᴘꜱ ✢', 'info')
+                InlineKeyboardButton(f'🧣 ɪɴꜰᴏ 🧣', 'tips'),
+                InlineKeyboardButton(f'🧸 ᴛɪᴘꜱ 🧸', 'info')
             ]
             )
                 
@@ -139,16 +139,16 @@ async def next_page(bot, query):
         if settings['auto_delete']:
             btn.insert(0, 
             [
-                InlineKeyboardButton(f'✢ ɪɴꜰᴏ ✢', 'tips'),
-                InlineKeyboardButton(f'✢ ᴛɪᴘꜱ ✢', 'info')
+                InlineKeyboardButton(f'🧣 ɪɴꜰᴏ 🧣', 'tips'),
+                InlineKeyboardButton(f'🧸 ᴛɪᴘꜱ 🧸', 'info')
             ]
             )
 
         else:
             btn.insert(0, 
             [
-                InlineKeyboardButton(f'✢ ɪɴꜰᴏ ✢', 'tips'),
-                InlineKeyboardButton(f'✢ ᴛɪᴘꜱ ✢', 'info')
+                InlineKeyboardButton(f'🧣 ɪɴꜰᴏ 🧣', 'tips'),
+                InlineKeyboardButton(f'🧸 ᴛɪᴘꜱ 🧸', 'info')
             ]
             )
     try:
@@ -244,7 +244,7 @@ async def next_page(bot, query):
         InlineKeyboardButton(f'🎬 {search} 🎬', 'rkbtn')
     ])
     btn.insert(2, [
-        InlineKeyboardButton("⎙ ꜱᴇɴᴅ ᴀʟʟ ꜰɪʟᴇꜱ ⎙", callback_data=f"send_all#{req}#{key}#{pre}")
+        InlineKeyboardButton("🔔 ꜱᴇɴᴅ ᴀʟʟ ꜰɪʟᴇꜱ 🔔", callback_data=f"send_all#{req}#{key}#{pre}")
     ])
     try:
         await query.edit_message_reply_markup(
@@ -539,7 +539,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                         file_id=file_id,
                         caption=f_caption,
                         protect_content=True if ident == "filep" else False,
-                        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('BOT UPDATES', url="https://t.me/ckflicks") ] ] ))
+                        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('BOT UPDATES', url="https://t.me/AM_FILMS") ] ] ))
                 else:
                     await query.answer(f"𝖧𝖾𝗒 {query.from_user.first_name}, 𝖳𝗁𝗂𝗌 𝗂𝗌 𝗇𝗈𝗍 𝗒𝗈𝗎𝗋 𝗋𝖾𝗊𝗎𝖾𝗌𝗍 !", show_alert=True)
                 await query.answer('𝖢𝗁𝖾𝖼𝗄 𝖯𝖬, 𝖨 𝗁𝖺𝗏𝖾 𝗌𝖾𝗇𝗍 𝖿𝗂𝗅𝖾𝗌 𝗂𝗇 𝖯𝖬', show_alert=True)
@@ -577,7 +577,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             file_id=file_id,
             caption=f_caption,
             protect_content=True if ident == 'checksubp' else False,
-            reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('BOT UPDATES', url="https://t.me/piroxbots") ] ] ))
+            reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('BOT UPDATES', url="https://t.me/AM_FILMS") ] ] ))
     elif query.data == "pages":
         await query.answer()
 
@@ -889,19 +889,19 @@ async def cb_handler(client: Client, query: CallbackQuery):
         await query.answer("𝗥𝗲𝗾𝘂𝗲𝘀𝘁𝘀 𝗙𝗼𝗿𝗺𝗮𝘁𝘀\n\n• 𝖲𝗈𝗅𝗈 2017\n• 𝖣𝗁𝗈𝗈𝗆 3 𝖧𝗂𝗇𝖽𝗂\n• 𝖪𝗎𝗋𝗎𝗉 𝖪𝖺𝗇𝗇𝖺𝖽𝖺\n• 𝖣𝖺𝗋𝗄 𝗌01\n• 𝖲𝗁𝖾 𝖧𝗎𝗅𝗄 720𝗉\n• 𝖥𝗋𝗂𝖾𝗇𝖽𝗌 𝗌03 1080𝗉\n\n‼️𝗗𝗼𝗻𝘁 𝗮𝗱𝗱 𝘄𝗼𝗿𝗱𝘀 & 𝘀𝘆𝗺𝗯𝗼𝗹𝘀  , . - 𝗹𝗶𝗸𝗲 send link movie series 𝗲𝘁𝗰‼️", True)
     
     elif query.data == 'tips':
-        await query.answer("𝖳𝗁𝗂𝗌 𝖬𝖾𝗌𝗌𝖺𝗀𝖾 𝖶𝗂𝗅𝗅 𝖡𝖾 𝖣𝖾𝗅𝖾𝗍𝖾𝖽 𝖠𝖿𝗍𝖾𝗋 5 𝖬𝗂𝗇𝗎𝗍𝖾𝗌 𝗍𝗈 𝖯𝗋𝖾𝗏𝖾𝗇𝗍 𝖢𝗈𝗉𝗒𝗋𝗂𝗀𝗁𝗍 !\n\n𝖳𝗁𝖺𝗇𝗄 𝖸𝗈𝗎 𝖥𝗈𝗋 𝖴𝗌𝗂𝗇𝗀 𝖬𝖾 😊\n\n\n𝖯𝗈𝗐𝖾𝗋𝖾𝖽 𝖡𝗒 𝖯𝖨𝖱𝖮", True)
+        await query.answer("𝖳𝗁𝗂𝗌 𝖬𝖾𝗌𝗌𝖺𝗀𝖾 𝖶𝗂𝗅𝗅 𝖡𝖾 𝖣𝖾𝗅𝖾𝗍𝖾𝖽 𝖠𝖿𝗍𝖾𝗋 5 𝖬𝗂𝗇𝗎𝗍𝖾𝗌 𝗍𝗈 𝖯𝗋𝖾𝗏𝖾𝗇𝗍 𝖢𝗈𝗉𝗒𝗋𝗂𝗀𝗁𝗍 !\n\n𝖳𝗁𝖺𝗇𝗄 𝖸𝗈𝗎 𝖥𝗈𝗋 𝖴𝗌𝗂𝗇𝗀 𝖬𝖾 😊\n\n\n𝖯𝗈𝗐𝖾𝗋𝖾𝖽 𝖡𝗒 @AM_FILMS", True)
 
     elif query.data == "start":
         buttons = [[
-                    InlineKeyboardButton('➕ᴀᴅᴅ ᴍᴇ ᴛᴏ ɢʀᴏᴜᴘ➕', url=f"http://t.me/{temp.U_NAME}?startgroup=true")
+                    InlineKeyboardButton('🎄 ᴀᴅᴅ ᴍᴇ ᴛᴏ ɢʀᴏᴜᴘ 🎄', url=f"http://t.me/{temp.U_NAME}?startgroup=true")
                 ],[
-                    InlineKeyboardButton('✢ ᴏᴡɴᴇʀ ✢', callback_data="owner_info"),
-                    InlineKeyboardButton('✢ ꜱᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ ✢', url=f"https://t.me/{SUPPORT_CHAT}")
+                    InlineKeyboardButton('☃︎ ᴏᴡɴᴇʀ ☃︎', callback_data="owner_info"),
+                    InlineKeyboardButton('🎁 ꜱᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ 🎁', url=f"https://t.me/{SUPPORT_CHAT}")
                 ],[
-                    InlineKeyboardButton('✢ ʜᴇʟᴘ ✢', callback_data='help'),
-                    InlineKeyboardButton('✢ ᴀʙᴏᴜᴛ ✢', callback_data='about'),
+                    InlineKeyboardButton('❄️ ʜᴇʟᴘ ❄️', callback_data='help'),
+                    InlineKeyboardButton('🥂 ᴀʙᴏᴜᴛ 🥂', callback_data='about'),
                 ],[
-                    InlineKeyboardButton('✢ ɪɴʟɪɴᴇ ꜱᴇᴀʀᴄʜ ✢', switch_inline_query_current_chat='')
+                    InlineKeyboardButton('🎀 ɪɴʟɪɴᴇ ꜱᴇᴀʀᴄʜ 🎀', switch_inline_query_current_chat='')
                   ]]
         
         reply_markup = InlineKeyboardMarkup(buttons)
@@ -979,7 +979,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         )
     elif query.data == "about":
         buttons = [[
-            InlineKeyboardButton('✢ ꜱᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ ✢', url=f"https://t.me/{SUPPORT_CHAT}"),
+            InlineKeyboardButton('🎁 ꜱᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ 🎁', url=f"https://t.me/{SUPPORT_CHAT}"),
             InlineKeyboardButton('✢ ꜱᴏᴜʀᴄᴇ ᴄᴏᴅᴇ✢', callback_data='source')
         ],[
             InlineKeyboardButton('✢ ʜᴏᴍᴇ ✢', callback_data='start'),
@@ -1318,16 +1318,16 @@ async def auto_filter(client, msg, spoll=False):
         if settings['auto_delete']:
             btn.insert(0, 
             [
-                InlineKeyboardButton(f'✢ ɪɴꜰᴏ ✢', 'tips'),
-                InlineKeyboardButton(f'✢ ᴛɪᴘꜱ ✢', 'info')
+                InlineKeyboardButton(f'🧣 ɪɴꜰᴏ 🧣', 'tips'),
+                InlineKeyboardButton(f'🧸 ᴛɪᴘꜱ 🧸', 'info')
             ]
             )
 
         else:
             btn.insert(0, 
             [
-                InlineKeyboardButton(f'✢ ɪɴꜰᴏ ✢', 'tips'),
-                InlineKeyboardButton(f'✢ ᴛɪᴘꜱ ✢', 'info')
+                InlineKeyboardButton(f'🧣 ɪɴꜰᴏ 🧣', 'tips'),
+                InlineKeyboardButton(f'🧸 ᴛɪᴘꜱ 🧸', 'info')
             ]
             )
                 
@@ -1338,16 +1338,16 @@ async def auto_filter(client, msg, spoll=False):
         if settings['auto_delete']:
             btn.insert(0, 
             [
-                InlineKeyboardButton(f'✢ ɪɴꜰᴏ ✢', 'tips'),
-                InlineKeyboardButton(f'✢ ᴛɪᴘꜱ ✢', 'info')
+                InlineKeyboardButton(f'🧣 ɪɴꜰᴏ 🧣', 'tips'),
+                InlineKeyboardButton(f'🧸 ᴛɪᴘꜱ 🧸', 'info')
             ]
             )
 
         else:
             btn.insert(0, 
             [
-                InlineKeyboardButton(f'✢ ɪɴꜰᴏ ✢', 'tips'),
-                InlineKeyboardButton(f'✢ ᴛɪᴘꜱ ✢', 'info')
+                InlineKeyboardButton(f'🧣 ɪɴꜰᴏ 🧣', 'tips'),
+                InlineKeyboardButton(f'🧸 ᴛɪᴘꜱ 🧸', 'info')
             ])
             
                       
@@ -1355,7 +1355,7 @@ async def auto_filter(client, msg, spoll=False):
         InlineKeyboardButton(f'🎬 {search} 🎬', 'rkbtn')
     ])
     btn.insert(2, [
-        InlineKeyboardButton("⎙ ꜱᴇɴᴅ ᴀʟʟ ꜰɪʟᴇꜱ ⎙", callback_data=f"send_all#{req}#{key}#{pre}")
+        InlineKeyboardButton("🔔 ꜱᴇɴᴅ ᴀʟʟ ꜰɪʟᴇꜱ 🔔", callback_data=f"send_all#{req}#{key}#{pre}")
     ])
     
     if offset != "":
