44e760c396b35d6da49aefa240ffceda469352ea
Kichu <kichumon333@outlook.com>
2024-12-22 13:25:43 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -70,7 +70,7 @@ async def give_filter(client, message):
 @Client.on_message(filters.private & filters.text & filters.incoming)
 async def pv_filter(client, message):
     kd = await global_filters(client, message)
-      if kd == False:
+    if kd == False:  # Fixed indentation
         # Send and delete a sticker
         sticker = await message.reply_sticker(sticker="CAACAgQAAxkBAAENXztnZ8DgxTDtXJ5YaSMhlCySiyzjHAAC1REAAqXq2VPT8nJumdJaaDYE")
         await sleep(2)
